91b4efdeeac016e51f7a0df1f8f0f16a40705378
Hayespace <ben.hayes@hotmail.com>
2023-04-13 20:47:42 +0000

    Add favicon and keydown events

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 
 });
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -49,10 +58,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculateAnswer[0];
 
     if (isCorrect) {
-        alert("Hey! You got it right! :D");
+        alert("Fuck Yeah! You got it right! :D");
         incrementScore();
     } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}`);
+        alert(`Tough Shit.... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}`);
         incrementWrongAnswer();
     }
 
