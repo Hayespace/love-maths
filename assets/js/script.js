1f78b4c4f9f0bca5907950de9b4abb419465ef4d
Hayespace <ben.hayes@hotmail.com>
2023-04-13 11:25:34 +0000

    Add correct and incorrect answer counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculateAnswer[1]);
@@ -71,10 +73,15 @@ function calculateCorrectAnswer() {
 
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
